31b4e626070ba0c2660932595145ff887e986860
Sydney <sbaroya@calpoly.edu>
2018-12-04 15:03:08 -0800

    added forgot password and dashboard activities

--- app/src/main/java/secapstone/helper/LoginActivity.java
+++ app/src/main/java/secapstone/helper/LoginActivity.java
@@ -1,5 +1,8 @@
 package secapstone.helper;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,6 +11,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import com.saga.communityhelperhandmade.R;
@@ -42,22 +49,44 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btnLogin.setOnClickListener(this);
 
         //init firebase auth
-//        mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         //check already session, if->dashboard
         //create dashboard activity
-        /*
-        * if(auth.getCurrentUser() != null)
-        *   startActivity(new Intent(LoginActivity.this, DashBoard.class));
-        * */
+
+        if(mAuth.getCurrentUser() != null)
+            startActivity(new Intent(LoginActivity.this, DashBoard.class));
+
 
     }
 
     @Override
     public void onClick(View v) {
-//        if(v.getId() == R.id.login_btn_forgot_password){
-//
-//        }
+        if(v.getId() == R.id.login_btn_forgot_password){
+            startActivity(new Intent(LoginActivity.this,ForgotPassword.class));
+            finish();
+        }
+        else if(v.getId() == R.id.login_btn_login){
+            loginUser(input_email.getText().toString(), input_password.getText().toString());
+        }
+    }
+
+    private void loginUser(String email, final String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            if(password.length()<6){
+                                Snackbar snackBar= Snackbar.make(activity_login, "Passowrd length must be over 6", Snackbar.LENGTH_SHORT);
+                                snackBar.show();
+                            }
+                        }
+                        else{
+                            startActivity(new Intent(LoginActivity.this, DashBoard.class));
+                        }
+                    }
+                });
     }
 }
 
