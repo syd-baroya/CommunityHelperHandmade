f3b4ff232600af42287bdef60eefc2a688e11ada
Addison Martin <addisonejmartin@gmail.com>
2018-12-06 12:48:58 -0800

    Back button in forgot password now works

--- app/src/main/java/secapstone/helper/ForgotPassword.java
+++ app/src/main/java/secapstone/helper/ForgotPassword.java
@@ -1,9 +1,14 @@
 package secapstone.helper;
 
+import android.content.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
 import com.saga.communityhelperhandmade.R;
+
+import secapstone.helper.addartisan.*;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -11,5 +16,20 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        Button backButton = (Button) findViewById(R.id.forgot_btn_back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                onClickReturnToLogin();
+            }
+        });
+
+    }
+
+    public void onClickReturnToLogin()
+    {
+        startActivity(new Intent(ForgotPassword.this, LoginActivity.class));
     }
 }
