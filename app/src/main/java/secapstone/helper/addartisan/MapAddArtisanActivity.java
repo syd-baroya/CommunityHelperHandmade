2bd173e81abf4e4f484306e17949f3ca6b72e4bb
Addison Martin <addisonejmartin@gmail.com>
2018-12-06 07:31:35 -0800

    You can now select the Artisan's address from google maps popup.

--- app/src/main/java/secapstone/helper/addartisan/MapAddArtisanActivity.java
+++ app/src/main/java/secapstone/helper/addartisan/MapAddArtisanActivity.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.*;
 
+import com.google.android.gms.common.*;
+import com.google.android.gms.location.places.*;
+import com.google.android.gms.location.places.ui.*;
 import com.saga.communityhelperhandmade.*;
+import com.saga.communityhelperhandmade.R;
 
 public class MapAddArtisanActivity extends AppCompatActivity
 {
     EditText adr;
+    final int PLACE_PICKER_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -39,6 +44,39 @@ public class MapAddArtisanActivity extends AppCompatActivity
             }
         });
         adr = (EditText)findViewById(R.id.artisanAddress);
+
+        Button selectOnMapButton = (Button) findViewById(R.id.selectOnMapButton);
+        selectOnMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            { onClickLaunchGoogleMaps();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                adr.setText(place.getLatLng().toString());
+            }
+        }
+    }
+
+    public void onClickLaunchGoogleMaps()
+    {
+        try
+        {
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+            //TODO Alert the user that Google maps isn't availible right now.
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        }
     }
 
     public void onClickNext5()
