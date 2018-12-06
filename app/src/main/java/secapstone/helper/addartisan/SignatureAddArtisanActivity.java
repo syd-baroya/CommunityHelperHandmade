753918d2a433d46934dc9a838189c0a707d7d98f
aliskoolyifico <35312417+aliskoolyifico@users.noreply.github.com>
2018-12-05 22:25:59 -0800

    al's add artisan

--- app/src/main/java/secapstone/helper/addartisan/SignatureAddArtisanActivity.java
+++ app/src/main/java/secapstone/helper/addartisan/SignatureAddArtisanActivity.java
@@ -1,18 +1,32 @@
 package secapstone.helper.addartisan;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.saga.communityhelperhandmade.*;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import secapstone.helper.Artisan;
 import secapstone.helper.MainActivity;
 
 public class SignatureAddArtisanActivity extends AppCompatActivity
 {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference artisansRef = db.collection("artisans");
+
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -53,7 +67,19 @@ public class SignatureAddArtisanActivity extends AppCompatActivity
     }
 
     public void pushArtisan(Artisan mewBoi){
-        //so here is where i would, ideally, push the info to the firebase
-        //but i dont know anything about that
+        db.collection("artisans")
+                .add(mewBoi)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>(){
+                    @Override
+                    public void onSuccess(DocumentReference documentReference){
+                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener(){
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 }
