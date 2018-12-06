b9cc8fa9d2d42059b9927e792cee14aab8d70be7
Addison Martin <addisonejmartin@gmail.com>
2018-12-06 09:50:25 -0800

    Working on adding preview

---
+++ app/src/main/java/secapstone/helper/addartisan/FinalPreviewAddArtisanActivity.java
@@ -0,0 +1,18 @@
+package secapstone.helper.addartisan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.saga.communityhelperhandmade.*;
+
+public class FinalPreviewAddArtisanActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_preview_add_artisan);
+    }
+}
