098b9c5fdb5ccc7a4eb91ba245560ba7d573c373
Addison Martin <addisonejmartin@gmail.com>
2018-12-06 10:05:33 -0800

    Working on adding a preview screen. Activity connected but it doesn't load preview yet

--- app/src/main/java/secapstone/helper/addartisan/FinalPreviewAddArtisanActivity.java
+++ app/src/main/java/secapstone/helper/addartisan/FinalPreviewAddArtisanActivity.java
@@ -1,12 +1,27 @@
 package secapstone.helper.addartisan;
 
+import android.content.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.firestore.*;
+import com.saga.communityhelperhandmade.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.*;
+import android.widget.*;
 
-import com.saga.communityhelperhandmade.*;
+import secapstone.helper.*;
+import secapstone.helper.Artisan;
+
 
 public class FinalPreviewAddArtisanActivity extends AppCompatActivity
 {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference artisansRef = db.collection("artisans");
+    private static final String TAG = "FinalPreviewActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -14,5 +29,53 @@ public class FinalPreviewAddArtisanActivity extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_preview_add_artisan);
+
+        Button nextButton8 = (Button) findViewById(R.id.nextButton8);
+        nextButton8.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                onClickNext8();
+            }
+        });
+
+        Button backButton8 = (Button) findViewById(R.id.backButton8);
+        backButton8.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                onClickBack8();
+            }
+        });
+    }
+
+    public void onClickNext8()
+    {
+        pushArtisan(WelcomeAddArtisanActivity.mew);
+        startActivity(new Intent(FinalPreviewAddArtisanActivity.this, MainActivity.class));
+    }
+
+    public void onClickBack8()
+    {
+        startActivity(new Intent(FinalPreviewAddArtisanActivity.this, SignatureAddArtisanActivity.class));
+    }
+
+    public void pushArtisan(Artisan mewBoi){
+        db.collection("artisans")
+            .add(mewBoi)
+            .addOnSuccessListener(new OnSuccessListener<DocumentReference>(){
+                @Override
+                public void onSuccess(DocumentReference documentReference){
+                    Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                }
+            })
+            .addOnFailureListener(new OnFailureListener(){
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w(TAG, "Error adding document", e);
+                }
+            });
     }
 }
--- app/src/main/java/secapstone/helper/addartisan/SignatureAddArtisanActivity.java
+++ app/src/main/java/secapstone/helper/addartisan/SignatureAddArtisanActivity.java
@@ -1,32 +1,17 @@
 package secapstone.helper.addartisan;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
 import com.saga.communityhelperhandmade.*;
 
-import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-
-import secapstone.helper.Artisan;
-import secapstone.helper.MainActivity;
 
 public class SignatureAddArtisanActivity extends AppCompatActivity
 {
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private CollectionReference artisansRef = db.collection("artisans");
 
-    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -57,8 +42,7 @@ public class SignatureAddArtisanActivity extends AppCompatActivity
 
     public void onNextClick7()
     {
-        pushArtisan(WelcomeAddArtisanActivity.mew);
-        startActivity(new Intent(SignatureAddArtisanActivity.this, MainActivity.class));
+        startActivity(new Intent(SignatureAddArtisanActivity.this, FinalPreviewAddArtisanActivity.class));
     }
 
     public void onClickBack7()
@@ -66,20 +50,5 @@ public class SignatureAddArtisanActivity extends AppCompatActivity
         startActivity(new Intent(SignatureAddArtisanActivity.this, DescriptionAddArtisanActivity.class));
     }
 
-    public void pushArtisan(Artisan mewBoi){
-        db.collection("artisans")
-                .add(mewBoi)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>(){
-                    @Override
-                    public void onSuccess(DocumentReference documentReference){
-                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener(){
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
-    }
+
 }
