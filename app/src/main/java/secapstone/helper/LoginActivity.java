dea380a0f4d6c80310e1f4c89c63020918fea7e6
syd <sbaroya@calpoly.edu>
2019-01-22 16:36:57 -0800

    fixed package bug and added login activity skeleton

--- app/src/main/java/secapstone/helper/LoginActivity.java
+++ app/src/main/java/secapstone/helper/LoginActivity.java
@@ -10,19 +10,17 @@ import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseAuthInvalidUserException;
-import com.google.firebase.auth.ProviderQueryResult;
+import com.amazon.identity.auth.device.AuthError;
+import com.amazon.identity.auth.device.api.Listener;
+import com.amazon.identity.auth.device.api.authorization.AuthCancellation;
+import com.amazon.identity.auth.device.api.authorization.AuthorizationManager;
+import com.amazon.identity.auth.device.api.authorization.AuthorizeListener;
+import com.amazon.identity.auth.device.api.authorization.AuthorizeRequest;
+import com.amazon.identity.auth.device.api.authorization.AuthorizeResult;
+import com.amazon.identity.auth.device.api.authorization.ProfileScope;
+import com.amazon.identity.auth.device.api.authorization.Scope;
+import com.amazon.identity.auth.device.api.workflow.RequestContext;
 import secapstone.helper.R;
 
 /**
@@ -33,15 +31,90 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
 
+    private RequestContext requestContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestContext = RequestContext.create(this);
 
+        requestContext.registerListener(new AuthorizeListener() {
+
+            /* Authorization was completed successfully. */
+            @Override
+            public void onSuccess(AuthorizeResult result) {
+                /* Your app is now authorized for the requested scopes */
+            }
+
+            /* There was an error during the attempt to authorize the
+            application. */
+            @Override
+            public void onError(AuthError ae) {
+                /* Inform the user of the error */
+            }
+
+            /* Authorization was cancelled before it could be completed. */
+            @Override
+            public void onCancel(AuthCancellation cancellation) {
+                /* Reset the UI to a ready-to-login state */
+            }
+        });
+
+        View loginButton = findViewById(R.id.login_with_amazon);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthorizationManager.authorize(new AuthorizeRequest
+                        .Builder(requestContext)
+                        .addScopes(ProfileScope.profile(), ProfileScope.postalCode())
+                        .build());
+            }
+        });
     }
 
     @Override
-    public void onClick(View v) {
+    protected void onResume() {
+        super.onResume();
+        requestContext.onResume();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Scope[] scopes = {
+                ProfileScope.profile(),
+                ProfileScope.postalCode()
+        };
+        AuthorizationManager.getToken(this, scopes, new Listener< AuthorizeResult, AuthError >() {
+
+            @Override
+            public void onSuccess(AuthorizeResult result) {
+                if (result.getAccessToken() != null) {
+                    /* The user is signed in */
+                } else {
+                    /* The user is not signed in */
+                }
+            }
+
+            @Override
+            public void onError(AuthError ae) {
+                /* The user is not signed in */
+            }
+        });
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        AuthorizationManager.signOut(getApplicationContext(), new Listener < Void, AuthError > () {
+            @Override
+            public void onSuccess(Void response) {
+                // Set logged out state in UI
+            }
+            @Override
+            public void onError(AuthError authError) {
+                // Log the error
+            }
+        });
     }
 
 }
