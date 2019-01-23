102a04a16b8dd4c8f1c3186b13aea2efce74f18f
syd <sbaroya@calpoly.edu>
2019-01-22 16:29:46 -0800

    finished up to creating login button

--- app/src/main/java/secapstone/helper/LoginActivity.java
+++ app/src/main/java/secapstone/helper/LoginActivity.java
@@ -31,96 +31,18 @@ import secapstone.helper.R;
  */
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button btnLogin;
-    EditText input_email, input_password;
-    TextView btnForgotPass;
 
-    ConstraintLayout activity_login;
-
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        //view
-        btnLogin = (Button)findViewById(R.id.login_btn_login);
-        input_email = (EditText)findViewById(R.id.login_email);
-        input_password = (EditText)findViewById(R.id.login_password);
-        btnForgotPass = (TextView)findViewById(R.id.login_btn_forgot_password);
-        activity_login = (ConstraintLayout)findViewById(R.id.activity_login);
-
-        btnForgotPass.setOnClickListener(this);
-        btnLogin.setOnClickListener(this);
-
-        //init firebase auth
-        mAuth = FirebaseAuth.getInstance();
-
-        //check already session, if->dashboard
-        //create dashboard activity
-//
-//        if(mAuth.getCurrentUser() != null)
-//            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-
 
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.login_btn_forgot_password){
-            startActivity(new Intent(LoginActivity.this,ForgotPassword.class));
-            finish();
-        }
-        else if(v.getId() == R.id.login_btn_login){
-            loginUser(input_email.getText().toString(), input_password.getText().toString());
-        }
+
     }
 
-
-    private void loginUser(String email, final String password){
-
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful()){
-                            if(isValidPasswordLength(password)){
-                                Snackbar snackBar= Snackbar.make(activity_login, "Password length must be over 6", Snackbar.LENGTH_SHORT);
-                                snackBar.show();
-                            }
-
-                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-                                /**
-                                 * Thrown when one or more of the credentials passed to a method fail to
-                                 * identify and/or authenticate the user subject of that operation.
-                                 * Inspect the error code and message to find out the specific cause.
-                                 * https://firebase.google.com/docs/reference/android/com/google/firebase/auth/FirebaseAuthInvalidCredentialsException
-                                 */
-
-                                Snackbar snackBar= Snackbar.make(activity_login, " Invalid credentials. Check email and password again.", Snackbar.LENGTH_SHORT);
-                                snackBar.show();
-                            } else if (task.getException() instanceof FirebaseAuthInvalidUserException) {
-                                /**
-                                 * Inspect the error code and message to find out the specific cause.
-                                 * https://firebase.google.com/docs/reference/android/com/google/firebase/auth/FirebaseAuthInvalidUserException
-                                 */
-                                Snackbar snackBar= Snackbar.make(activity_login, " Invalid credentials. Check email and password again.", Snackbar.LENGTH_SHORT);
-                                snackBar.show();
-                            }
-
-                        }
-                        else{
-                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                        }
-                    }
-                });
-    }
-
-    public boolean isValidPasswordLength(String password){
-        if(password.length()<6)
-            return false;
-        return true;
-    }
 }
 
