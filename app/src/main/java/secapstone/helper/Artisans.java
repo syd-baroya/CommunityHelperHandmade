166f3259a1f5433ab9fb97c2e23b07bb4ec14fcc
spoorthyv <sckortyman@gmail.com>
2019-01-30 03:00:40 -0800

    Add super shitty search. Only searches names. Names need to match exactly.

--- app/src/main/java/secapstone/helper/Artisans.java
+++ app/src/main/java/secapstone/helper/Artisans.java
@@ -1,8 +1,8 @@
 package secapstone.helper;
 
 
-import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,14 +11,12 @@ import android.view.*;
 import android.widget.*;
 import secapstone.helper.R;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
-import java.util.ArrayList;
-
-import secapstone.helper.addartisan.WelcomeAddArtisanActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,9 +25,13 @@ public class Artisans extends Fragment {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference artisansRef = db.collection("artisans");
-    private ArtisanAdapter adapter;
+    private FirestoreRecyclerAdapter adapter;
 
     private View view;
+
+    TextView artisanSearchField;
+    Button searchButton;
+    RecyclerView recyclerView;
 
 
     public Artisans() {
@@ -38,30 +40,54 @@ public class Artisans extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_artisans, container, false);
-
-        setUpRecyclerView();
 
         return view;
     }
 
-    private void setUpRecyclerView() {
-        Query query = artisansRef.orderBy("name", Query.Direction.DESCENDING);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        artisanSearchField = (TextView) view.findViewById(R.id.searchArtisanField);
+        searchButton = (Button) view.findViewById(R.id.searchArtisanButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseSearchArtisans();
+            }
+        });
+        recyclerView = view.findViewById(R.id.artisan_recycler_view);
+
+        firebaseSearchArtisans();
+
+    }
+
+    private void firebaseSearchArtisans() {
+        if (adapter != null) {
+            adapter.stopListening();
+        }
+
+        Query query;
+        String search = artisanSearchField.getText().toString();
+
+        if (search.length() < 1) {
+            query = artisansRef.orderBy("name", Query.Direction.ASCENDING);
+        } else {
+            query = artisansRef.whereEqualTo("name", search).orderBy("name", Query.Direction.ASCENDING);
+        }
+
         FirestoreRecyclerOptions<Artisan> options = new FirestoreRecyclerOptions.Builder<Artisan>()
                 .setQuery(query, Artisan.class)
                 .build();
 
         adapter = new ArtisanAdapter(options, getContext());
 
-        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        adapter.startListening();
     }
-
 
 
     @Override
@@ -75,4 +101,5 @@ public class Artisans extends Fragment {
         super.onStop();
         adapter.stopListening();
     }
+
 }
