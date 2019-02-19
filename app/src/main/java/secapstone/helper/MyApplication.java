047d1876fc5d636e6384b9f3bbe3101b79eb9f28
syd-baroya <sbaroya@calpoly.edu>
2019-02-19 14:57:10 -0800

    trying to fix firebase bug

---
+++ app/src/main/java/secapstone/helper/MyApplication.java
@@ -0,0 +1,13 @@
+package secapstone.helper;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
+}
