42ddb9befa5d18e810b3d889dcf33b77541ae28d
spoorthyv <sckortyman@gmail.com>
2019-02-26 00:53:57 -0800

    Redo popup for contact info.

--- app/src/main/java/secapstone/helper/ViewArtisanActivity.java
+++ app/src/main/java/secapstone/helper/ViewArtisanActivity.java
@@ -1,26 +1,33 @@
 package secapstone.helper;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import secapstone.helper.R;
 
 public class ViewArtisanActivity extends AppCompatActivity {
+    private static final int REQUEST_CALL = 1;
 
     private StorageReference storageRef = FirebaseStorage.getInstance().getReference();
     public static String artisanName;
@@ -28,6 +35,8 @@ public class ViewArtisanActivity extends AppCompatActivity {
     public static String artisanPhone;
     public static String EXTRA_A_ADDRESS = "com.example.application.example.artisanAddress";
     public static String EXTRA_A_PHONE = "com.example.application.example.artisanPhone";
+
+    Dialog myDialog;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,6 +53,8 @@ public class ViewArtisanActivity extends AppCompatActivity {
                 onClickLogPayments(view);
             }
         });
+
+        myDialog = new Dialog(this);
 
     }
 
@@ -117,9 +128,60 @@ public class ViewArtisanActivity extends AppCompatActivity {
 
     public void onClickContactInfoButton(View view)
     {
-        Intent intent = new Intent(ViewArtisanActivity.this, ViewContactInfoActivity.class);
-        intent.putExtra(EXTRA_A_ADDRESS, artisanAddress);
-        intent.putExtra(EXTRA_A_PHONE, artisanPhone);
-        startActivity(intent);
+        myDialog.setContentView(R.layout.view_contact_info_modal);
+
+        if (artisanPhone != null) {
+            TextView phoneNumText = myDialog.findViewById(R.id.artisan_phone2);
+            phoneNumText.setText(artisanPhone);
+        }
+
+        if (artisanAddress != null) {
+            TextView addressText = myDialog.findViewById(R.id.artisan_address);
+            addressText.setText(artisanAddress);
+        }
+
+        myDialog.findViewById(R.id.callNowButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickCallButton();
+            }
+        });
+
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
+    }
+
+    public void onClickCloseContactInfo(View view)
+    {
+        myDialog.dismiss();
+    }
+
+    public void onClickDirectionsButton()
+    {
+        //startActivity(new Intent(ViewArtisanActivity.this, ViewReportsActivity.class));
+    }
+
+    public void onClickCallButton() {
+        if (ContextCompat.checkSelfPermission(myDialog.getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(myDialog.getOwnerActivity(), new String[] {Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        }
+        else {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:"+artisanPhone));
+            startActivity(intent);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                onClickCallButton();
+            }
+            else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
