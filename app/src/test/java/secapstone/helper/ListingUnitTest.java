18f8d11096d7de0089b36d7386d0a0e545374d72
Addison Martin <addisonejmartin@gmail.com>
2019-05-30 05:16:01 -0700

    Done adding unit tests

--- app/src/test/java/secapstone/helper/ListingUnitTest.java
+++ app/src/test/java/secapstone/helper/ListingUnitTest.java
@@ -1,14 +1,42 @@
 package secapstone.helper;
 import org.junit.Test;
 
+import secapstone.helper.model.Listing;
+
 import static org.junit.Assert.assertEquals;
 
 
 public class ListingUnitTest
 {
     @Test
-    public void defaultBalance_isCorrect()
+    public void title_isCorrect()
     {
+        Listing l = new Listing();
+        l.setTitle("Test title");
+        assertEquals("Test title", l.getTitle());
+    }
 
+    @Test
+    public void description_isCorrect()
+    {
+        Listing l = new Listing();
+        l.setDescription("Test desc");
+        assertEquals("Test desc", l.getDescription());
+    }
+
+    @Test
+    public void price_isCorrect()
+    {
+        Listing l = new Listing();
+        l.setPrice(10f);
+        assertEquals(10f, l.getPrice(), 0.001f);
+    }
+
+    @Test
+    public void shippedCount_isCorrect()
+    {
+        Listing l = new Listing();
+        l.setShippedCount(11);
+        assertEquals(11, l.getShippedCount());
     }
 }
