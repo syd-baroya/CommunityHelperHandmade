b43bd35a0ed4a9bc23beeb6a5d50c17ba024c4f6
Addison Martin <addisonejmartin@gmail.com>
2019-05-30 02:10:29 -0700

    Adding more unit tests

---
+++ app/src/test/java/secapstone/helper/ListingUnitTest.java
@@ -0,0 +1,14 @@
+package secapstone.helper;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ListingUnitTest
+{
+    @Test
+    public void defaultBalance_isCorrect()
+    {
+
+    }
+}
