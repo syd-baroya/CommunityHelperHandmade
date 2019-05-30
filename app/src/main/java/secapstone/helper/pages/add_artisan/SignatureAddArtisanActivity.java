ac28f3c603fb2ee267004acf5855ebe084eb1c2c
Addison Martin <addisonejmartin@gmail.com>
2019-05-30 04:51:16 -0700

    You now must input artisan's signature before continue button is enabled

--- app/src/main/java/secapstone/helper/pages/add_artisan/SignatureAddArtisanActivity.java
+++ app/src/main/java/secapstone/helper/pages/add_artisan/SignatureAddArtisanActivity.java
@@ -3,7 +3,11 @@ package secapstone.helper.pages.add_artisan;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+
+import com.kyanogen.signatureview.SignatureView;
 
 import secapstone.helper.R;
 
@@ -16,6 +20,16 @@ public class SignatureAddArtisanActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signature_add_artisan);
+
+        SignatureView sigView = (SignatureView) findViewById(R.id.signature_view);
+        sigView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Button nextButton = findViewById(R.id.nextButton);
+                nextButton.setEnabled(true);
+                return false;
+            }
+        });
     }
 
     public void onClickNext(View view)
@@ -26,8 +40,11 @@ public class SignatureAddArtisanActivity extends AppCompatActivity
 
     public void onClickBack(View view)
     {
-        startActivity(new Intent(SignatureAddArtisanActivity.this, DescriptionAddArtisanActivity.class));
-        overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+        SignatureView sigView = (SignatureView) findViewById(R.id.signature_view);
+        if (!sigView.isBitmapEmpty()) {
+            startActivity(new Intent(SignatureAddArtisanActivity.this, DescriptionAddArtisanActivity.class));
+            overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+        }
     }
 
 
