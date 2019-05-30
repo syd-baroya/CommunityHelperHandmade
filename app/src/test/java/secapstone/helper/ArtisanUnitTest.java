7aba58462eecead8109d49084c2a155540352473
Addison Martin <addisonejmartin@gmail.com>
2019-05-30 01:57:54 -0700

    Finished adding unit tests for artisan

--- app/src/test/java/secapstone/helper/ArtisanUnitTest.java
+++ app/src/test/java/secapstone/helper/ArtisanUnitTest.java
@@ -14,4 +14,60 @@ public class ArtisanUnitTest {
         a.setName("Test name");
         assertEquals("Test name", a.getName());
     }
+
+    @Test
+    public void description_isCorrect()
+    {
+        Artisan a = new Artisan();
+        a.setDescription("Test desc");
+        assertEquals("Test desc", a.getDescription());
+    }
+
+    @Test
+    public void phoneNumber_isCorrect()
+    {
+        Artisan a = new Artisan();
+        a.setPhoneNumber("111");
+        assertEquals("111", a.getPhoneNumber());
+    }
+
+    @Test
+    public void address_isCorrect()
+    {
+        Artisan a = new Artisan();
+        a.setAddress("111 St.");
+        assertEquals("111 St.", a.getAddress());
+    }
+
+    @Test
+    public void countryRegion_isCorrect()
+    {
+        Artisan a = new Artisan();
+        a.setCountryRegion("test");
+        assertEquals("test", a.getCountryRegion());
+    }
+
+    @Test
+    public void zipPostalCode_isCorrect()
+    {
+        Artisan a = new Artisan();
+        a.setZipPostalCode("11111");
+        assertEquals("11111", a.getZipPostalCode());
+    }
+
+    @Test
+    public void firstName_isCorrect()
+    {
+        Artisan a = new Artisan();
+        a.setFirstName("John");
+        assertEquals("John", a.getFirstName());
+    }
+
+    @Test
+    public void lastName_isCorrect()
+    {
+        Artisan a = new Artisan();
+        a.setLastName("Smith");
+        assertEquals("Smith", a.getLastName());
+    }
 }
