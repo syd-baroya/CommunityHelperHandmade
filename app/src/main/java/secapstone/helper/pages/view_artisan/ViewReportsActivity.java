5e1d13d33ed5fca96a0aec48bc55bbd1337471c8
syd-baroya <sbaroya@calpoly.edu>
2019-05-30 01:40:21 -0700

    put transaction data into table

--- app/src/main/java/secapstone/helper/pages/view_artisan/ViewReportsActivity.java
+++ app/src/main/java/secapstone/helper/pages/view_artisan/ViewReportsActivity.java
@@ -6,20 +6,20 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.opencsv.CSVWriter;
@@ -28,9 +28,12 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import secapstone.helper.R;
+import secapstone.helper.model.PayoutTransaction;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -39,16 +42,19 @@ public class ViewReportsActivity extends AppCompatActivity
 {
     private String artisanID="";
     private String cgaID="";
-    private FirebaseFirestore db;
+
     private static final String TAG = "ViewReportsActivity";
-    private List<Object[]> payoutData, productData, shipmentData;
-    private View view;
+    private List<Map<String, Object>> transactionData;
+    private Map<String, Object> map;
+    private ListView listView;
+    private PayoutTransaction payoutTransaction;
+    private ArrayAdapter<Map<String, Object>> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_reports);
-
+        listView = findViewById(R.id.listView);
 
         if (!checkPermission()) {
             openActivity();
@@ -78,8 +84,8 @@ public class ViewReportsActivity extends AppCompatActivity
                     && ActivityCompat.shouldShowRequestPermissionRationale(this, READ_EXTERNAL_STORAGE)) {
                 AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
                 alertBuilder.setCancelable(true);
-//                alertBuilder.setTitle(getString(R.string.permission_necessary));
-//                alertBuilder.setMessage(R.string.storage_permission_is_necessary_to_wrote_event);
+                alertBuilder.setTitle("CSV Download Status");
+                alertBuilder.setMessage("CSV file successfully downloaded!");
                 alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
                     public void onClick(DialogInterface dialog, int which) {
@@ -126,21 +132,81 @@ public class ViewReportsActivity extends AppCompatActivity
     }
 
     private void openActivity() {
-        //add your further process after giving permission or to download images from remote server.
-        payoutData = new ArrayList<>();
-        productData = new ArrayList<>();
-        shipmentData = new ArrayList<>();
-
+        FirebaseFirestore db;
+        transactionData = new ArrayList<>();
+        map = new HashMap<>();
+        adapter = new ArrayAdapter<>(this, R.layout.report_item, R.id.reportInfo, transactionData);
 
         getIncomingIntent();
         db = FirebaseFirestore.getInstance();
+
         if(artisanID.length() > 0)
-            getArtisanTransactions();
-        else if(cgaID.length()>0)
-            getCGATransactions();
+            getTransactions("artisanID", artisanID, db);
+//        else if(cgaID.length()>0)
+//            getCGATransactions();
     }
 
-    private void writeToCSV(List<Object[]> dataList)
+    private void getTransactions(String fieldName, String id, FirebaseFirestore db)
+    {
+        CollectionReference ref;
+        ref = db.collection("PayoutTransactions");
+        ref.whereEqualTo(fieldName, id)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        for (QueryDocumentSnapshot doc : value) {
+                            map = doc.getData();
+                            transactionData.add(map);
+                        }
+                        listView.setAdapter(adapter);
+
+                    }
+                });
+        ref = db.collection("ProductTransactions");
+        ref.whereEqualTo(fieldName, id)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        for (QueryDocumentSnapshot doc : value) {
+                            map = doc.getData();
+                            transactionData.add(map);
+                        }
+                        listView.setAdapter(adapter);
+                    }
+                });
+        ref = db.collection("Shipments");
+        ref.whereEqualTo(fieldName, id)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        for (QueryDocumentSnapshot doc : value) {
+                            map = doc.getData();
+                            transactionData.add(map);
+                        }
+                        listView.setAdapter(adapter);
+                    }
+                });
+    }
+
+    private void writeToCSV()
     {
         String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
         String fileName = "AnalysisData.csv";
@@ -188,186 +254,6 @@ public class ViewReportsActivity extends AppCompatActivity
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private void init() {
-        View view = findViewById(R.id.view_report_container);
-        TableLayout ll = view.findViewById(R.id.table);
-
-
-        for (int i = 0; i < payoutData.size(); i++) {
-
-            TableRow row = new TableRow(this);
-            TableLayout.LayoutParams tableRowParams=
-                    new TableLayout.LayoutParams
-                            (TableLayout.LayoutParams.FILL_PARENT,TableLayout.LayoutParams.WRAP_CONTENT);
-
-            int leftMargin=2;
-            int topMargin=2;
-            int rightMargin=2;
-            int bottomMargin=2;
-
-            tableRowParams.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
-
-            //row.setLayoutParams(tableRowParams);
-            TextView addressText = new TextView(this);
-            TextView amountText = new TextView(this);
-            TextView dateText = new TextView(this);
-            addressText.setLayoutParams(tableRowParams);
-            amountText.setLayoutParams(tableRowParams);
-//            String[] map = payoutData.get(i);
-//            String address = map[0];
-//            String amount = map[1];
-//            addressText.setText(address);
-//            amountText.setText(amount);
-            row.addView(addressText);
-            row.addView(amountText);
-            ll.addView(row, i);
-        }
-//        for (int i = 0; i < productData.size(); i++) {
-//
-//            TableRow row = new TableRow(this);
-//            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-//            row.setLayoutParams(lp);
-//            TextView productText = new TextView(this);
-//            TextView amountText = new TextView(this);
-//            TextView dateText = new TextView(this);
-//            Map<String, Object> map = productData.get(i);
-//            String product = map.get("productID").toString();
-//            String amount = map.get("amount").toString();
-//            productText.setText(product);
-//            amountText.setText(amount);
-//            row.addView(productText);
-//            row.addView(amountText);
-//            ll.addView(row, i);
-//        }
-//        for (int i = 0; i < shipmentData.size(); i++) {
-//
-//            TableRow row = new TableRow(this);
-//            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-//            row.setLayoutParams(lp);
-//            TextView productText = new TextView(this);
-//            TextView amountText = new TextView(this);
-//            TextView dateText = new TextView(this);
-//            Map<String, Object> map = shipmentData.get(i);
-//            String product = map.get("productID").toString();
-//            String amount = map.get("amount").toString();
-//            productText.setText(product);
-//            amountText.setText(amount);
-//            row.addView(productText);
-//            row.addView(amountText);
-//            ll.addView(row, i);
-//        }
-    }
-
-    private void getArtisanTransactions()
-    {
-        db.collection("PayoutTransactions")
-                .whereEqualTo("artisanID", artisanID)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Object[] values = document.getData().values().toArray(new Object[0]);
-                                payoutData.add(values);
-//                                Log.d(TAG, "Payout Transactions: " + document.getId() + " => " + document.getData());
-                            }
-                            writeToCSV(payoutData);
-
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
-//        db.collection("ProductTransactions")
-//                .whereEqualTo("artisanID", artisanID)
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                productData.add(document.getData());
-////                                Log.d(TAG, "Product Transactions: " + document.getId() + " => " + document.getData());
-//                            }
-//                        } else {
-//                            Log.d(TAG, "Error getting documents: ", task.getException());
-//                        }
-//                    }
-//                });
-//        db.collection("Shipments")
-//                .whereEqualTo("artisanID", artisanID)
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                shipmentData.add(document.getData());
-////                                Log.d(TAG, "Shipments: " + document.getId() + " => " + document.getData());
-//                            }
-//                        } else {
-//                            Log.d(TAG, "Error getting documents: ", task.getException());
-//                        }
-//                    }
-//                });
-    }
-
-    private void getCGATransactions()
-    {
-//        db.collection("PayoutTransactions")
-//                .whereEqualTo("cgaID", cgaID)
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                String[] values = document.getData().values().toArray(new String[0]);
-//                                payoutData.add(values);
-////                                Log.d(TAG, "Payout Transactions: " + document.getId() + " => " + document.getData());
-//                            }
-//                        } else {
-//                            Log.d(TAG, "Error getting documents: ", task.getException());
-//                        }
-//                    }
-//                });
-//        db.collection("ProductTransactions")
-//                .whereEqualTo("cgaID", cgaID)
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                String[] values = document.getData().values().toArray(new String[0]);
-//                                productData.add(values);
-////                                Log.d(TAG, "Product Transactions: " + document.getId() + " => " + document.getData());
-//                            }
-//                        } else {
-//                            Log.d(TAG, "Error getting documents: ", task.getException());
-//                        }
-//                    }
-//                });
-//        db.collection("Shipments")
-//                .whereEqualTo("cgaID", cgaID)
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                String[] values = document.getData().values().toArray(new String[0]);
-//                                shipmentData.add(values);
-////                                Log.d(TAG, "Shipments: " + document.getId() + " => " + document.getData());
-//                            }
-//                        } else {
-//                            Log.d(TAG, "Error getting documents: ", task.getException());
-//                        }
-//                    }
-//                });
     }
 
     private void getIncomingIntent() {
